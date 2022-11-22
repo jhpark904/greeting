8714f4ae7e694534bbe954a5e8f5c6819f9f730a
Jung Hwan Park <jhpark904@gmail.com>
2022-11-22 18:52:39 -0500

    make some edits

--- hello.test.js
+++ hello.test.js
@@ -4,3 +4,7 @@ var hello = utils.load('./hello.js', 'hello');
 test('outputs the correct string', () => {
   expect(hello()).toBe("Hello World!");
 });
+
+test('does not output bye world', () => {
+  expect(hello()).not.toBe("Bye World!")
+});
